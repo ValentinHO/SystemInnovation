b7ec75e58801e0c0e55c7889afb0b90485a265bd
ValentinHO <valentinhernandezortega@gmail.com>
2017-06-26 12:33:00 -0500

    Vista servicios 26-06-2017

--- services.php
+++ services.php
@@ -13,28 +13,48 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>System Innovation</title>
-    <meta charset="utf-8">
-    <link rel="icon" href="img/favicon.ico">
-    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1">
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-    <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" type="text/css" href="css/style.css">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>System Innovation</title>
+<link rel="icon" href="img/favicon.ico">
+
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link href="css/pagination.css" rel="stylesheet" type="text/css">
+<link href="css/styles.css" rel="stylesheet">
+
+<!--Icons-->
+<script src="js/lumino.glyphs.js"></script>
+
+<!--[if lt IE 9]>
+<script src="js/html5shiv.js"></script>
+<script src="js/respond.min.js"></script>
+<![endif]-->
+
 </head>
+
 <body id="serviciosmecanicos">
 
-<?php require('header.php');?>
+    <?php require('header.php');?>  
+    <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">           
+        <div class="row">
+            <ol class="breadcrumb">
+                <li><a href="main.php"><svg class="glyph stroked home"><use xlink:href="#stroked-home"></use></svg></a></li>
+                <li class="active">Servicios</li>
+            </ol>
+        </div><!--/.row-->
+        
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">Gestión de Servicios</h1>
+            </div>
+        </div><!--/.row-->
 
-<section id="contenido">
-    <article>
-        <div class="title-modulo">
-            <p>Gestión de Servicios Mecánicos</p>
-        </div>
 
-        <div class="alert alert-success hidden" id="messages"></div>
+        <div class="alert bg-success hidden" id="messages"></div>
 
         <div class="col-lg-12">
-            <div class="panel panel-default">
+            <div class="panel panel-primary">
                 <div class="panel-heading"><span class="glyphicon glyphicon-th-list"></span> Servicios</div>
 
                 <div class="panel-body">
@@ -83,17 +103,18 @@
 
                 </div>
             </div>
-        </div>                  
+        </div>
 
-    </article>
-</section>
+        
+        
+    </div>  <!--/.main-->
 
-<script src="js/jquery-3.1.1.min.js" type="text/javascript"></script>
-<script src="js/bootstrap.min.js" type="text/javascript"></script>
-<script src="js/bootbox.min.js" type="text/javascript"></script>
-<script src="js/services.js" type="text/javascript"></script>
-<script src="js/logout.js" type="text/javascript"></script>
-<script src="js/destroysessiontimeout.js" type="text/javascript"></script>
-
+    <script src="js/jquery-1.11.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/bootbox.min.js" type="text/javascript"></script>
+    <script src="js/services.js" type="text/javascript"></script>
+    <script src="js/logout.js" type="text/javascript"></script>
+    <script src="js/destroysessiontimeout.js" type="text/javascript"></script>
 </body>
+
 </html>
