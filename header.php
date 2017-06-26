7d3dea4e95fac204fec7d1678775e572f2aa627a
ValentinHO <valentinhernandezortega@gmail.com>
2017-06-26 12:29:53 -0500

    Cabecera principal 26-06-2017

--- header.php
+++ header.php
@@ -1,29 +1,36 @@
-<header class="menu" >
-	<input type="checkbox" id="toggle" checked value="6887" >
-	
-	<div class="icon-menu le">
-		<label for="toggle"><img src="img/menu.png"></label>
-	</div>
-	
-	<div class="logo-header le">
-		System Innovation
-	</div>
-
-	<div class="usernamemenu le">
-		Bienvenido <?php echo $_SESSION['nombreUsuario']; ?>
-		<label class="letter-main"><?php echo substr($_SESSION['nombreUsuario'],0,1); ?></label>
-		<a href="#" id="logout"><i class="fa fa-sign-out fa-fw"></i> Salir</a>
-	</div>
-
-	<nav class="menu-lateral" id="menu-lateral">
-		<ul>
-			<li><a href="main.php"><i class="fa fa-home fa-fw"></i> Inicio</a></li>
-			<li><a href="mechanics.php"><i class="fa fa-user fa-fw"></i> Mecánicos</a></li>
-			<li><a href="tips.php"><i class="fa fa-info fa-fw"></i> Tips</a></li>
-			<li><a href="services.php"><i class="fa fa-wrench fa-fw"></i> Servicios</a></li>
-			<li><a href="reports.php"><i class="fa fa-book fa-fw"></i> Reportes</a></li>
-			<!--<li><a href="settings.php"><i class="fa fa-gear fa-fw"></i> Configuración</a></li>-->
-			<li class="hidden-logout"><a href="#" id="logout"><i class="fa fa-sign-out fa-fw"></i> Salir</a></li>
-		</ul>
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#sidebar-collapse">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#"><span>SYSTEM</span>INNOVATION</a>
+				<ul class="user-menu">
+					<li class="dropdown pull-right">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown"><svg class="glyph stroked male-user"><use xlink:href="#stroked-male-user"></use></svg> Bienvenido <?php echo $_SESSION['nombreUsuario']; ?> <span class="caret"></span></a>
+						<ul class="dropdown-menu" role="menu">
+							<li><a href="#"><svg class="glyph stroked male-user"><use xlink:href="#stroked-male-user"></use></svg> Profile</a></li>
+							<li><a href="#"><svg class="glyph stroked gear"><use xlink:href="#stroked-gear"></use></svg> Settings</a></li>
+							<li><a href="#" id="logout"><svg class="glyph stroked cancel"><use xlink:href="#stroked-cancel"></use></svg> Salir</a></li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+							
+		</div><!-- /.container-fluid -->
 	</nav>
-</header>
+		
+	<div id="sidebar-collapse" class="col-sm-3 col-lg-2 sidebar">
+		
+		<ul class="nav menu">
+			<li><a href="main.php"><svg class="glyph stroked home"><use xlink:href="#stroked-home"/></svg> Inicio</a></li>
+			<li><a href="mechanics.php"><svg class="glyph stroked male user "><use xlink:href="#stroked-male-user"/></svg> Mecánicos</a></li>
+			<li><a href="tips.php"><svg class="glyph stroked eye"><use xlink:href="#stroked-eye"/></svg> Tips</a></li>
+			<li><a href="services.php"><svg class="glyph stroked table"><use xlink:href="#stroked-table"></use></svg> Servicios</a></li>
+			<li><a href="reports.php"><svg class="glyph stroked line-graph"><use xlink:href="#stroked-line-graph"></use></svg> Reportes</a></li>
+		</ul>
+
+	</div><!--/.sidebar-->
