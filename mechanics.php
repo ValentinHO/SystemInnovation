22925c7f905552edb28e1502de7a49a66098583e
ValentinHO <valentinhernandezortega@gmail.com>
2017-06-26 12:32:06 -0500

    Mecanicos 26-06-2017

--- mechanics.php
+++ mechanics.php
@@ -13,28 +13,47 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>System Innovation</title>
-    <meta charset="utf-8">
-    <link rel="icon" href="img/favicon.ico">
-    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1">
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-    <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" type="text/css" href="css/style.css">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>System Innovation</title>
+<link rel="icon" href="img/favicon.ico">
+
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link href="css/pagination.css" rel="stylesheet" type="text/css">
+<link href="css/styles.css" rel="stylesheet">
+
+<!--Icons-->
+<script src="js/lumino.glyphs.js"></script>
+
+<!--[if lt IE 9]>
+<script src="js/html5shiv.js"></script>
+<script src="js/respond.min.js"></script>
+<![endif]-->
+
 </head>
+
 <body id="mecanicos">
+    <?php require('header.php');?>  
+    <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">           
+        <div class="row">
+            <ol class="breadcrumb">
+                <li><a href="main.php"><svg class="glyph stroked home"><use xlink:href="#stroked-home"></use></svg></a></li>
+                <li class="active">Mecánicos</li>
+            </ol>
+        </div><!--/.row-->
+        
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">Gestión de Mecánicos</h1>
+            </div>
+        </div><!--/.row-->
 
-<?php require('header.php');?>
 
-<section id="contenido">
-    <article>
-        <div class="title-modulo">
-            <p>Gestión de Mecánicos</p>
-        </div>
-
-        <div class="alert alert-success hidden" id="messages"></div>
+        <div class="alert bg-success hidden" id="messages"></div>
 
         <div class="col-lg-12">
-            <div class="panel panel-default">
+            <div class="panel panel-primary">
                 <div class="panel-heading"><span class="glyphicon glyphicon-th-list"></span> Mecánicos</div>
 
                 <div class="panel-body">
@@ -55,77 +74,86 @@
                         <!--<div class="col-md-3 hidden" id="one"></div>-->
                         <div class="col-sm-12 hidden" id="addMechanic">
                             <div class="col-sm-12">
-                                <form method="POST" id="mechanic-form" name="mecanicosf">
-
-                                    <div class="col-md-6">
-                                        <div class="form-group">
-                                            <label for="mechanicname">Nombre de Mecánico</label>
-                                            <input type="text" name="mechanicname" id="mechanicname" class="form-control" placeholder="Nombre de Mecánico">
-                                            <div class="text-danger" id="div-nameM"></div>
-                                        </div>
-
-                                        <div class="form-group">
-                                            <label for="lastname">Apellido(s)</label>
-                                            <input type="text" name="lastname" id="lastname" class="form-control" placeholder="Apellido(s)">
-                                            <div class="text-danger" id="div-lastnameM"></div>
-                                        </div>
-
-                                        <div class="form-group">
-                                            <label for="phone">Teléfono</label>
-                                            <input type="text" name="phone" id="phone" class="form-control" placeholder="Teléfono" maxlength="10">
-                                            <div class="text-danger" id="div-phoneM"></div>
-                                        </div>
-                                    </div>
-
-                                    <div class="col-md-6">
-                                        
-                                        <fieldset>
-                                            <legend>Localización del Mecánico</legend>
+                                <form method="POST" enctype="multipart/form-data" id="mechanic-form" name="mecanicosf">
+                                    <div class="col-sm-12">
+                                        <div class="col-md-6">
                                             <div class="form-group">
-                                                <label for="lat">Latitud</label>
-                                                <input type="text" name="lat" id="lat" class="form-control" placeholder="Latitud" maxlength="15">
-                                                <div class="text-danger" id="div-latM"></div>
+                                                <label for="mechanicname">Nombre de Mecánico</label>
+                                                <input type="text" name="mechanicname" id="mechanicname" class="form-control" placeholder="Nombre de Mecánico">
+                                                <div class="text-danger" id="div-nameM"></div>
                                             </div>
 
                                             <div class="form-group">
-                                            <label for="lon">Longitud</label>
-                                                <input type="text" name="lon" id="lon" class="form-control" placeholder="Longitud" maxlength="15">
-                                                <div class="text-danger" id="div-lonM"></div>
+                                                <label for="lastname">Apellido(s)</label>
+                                                <input type="text" name="lastname" id="lastname" class="form-control" placeholder="Apellido(s)">
+                                                <div class="text-danger" id="div-lastnameM"></div>
                                             </div>
-                                            <div class="btn-group">
-                                                <button type="button" class="btn-inf dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                    <span class="fa fa-info"></span>
-                                                </button>
-                                                <ul class="dropdown-menu">
-                                                    <li><a href="#">El formato para la longitud y latitud debe ser como lo siguiente:<br>1. 00.0000000000 (máximo 10 decimales)<br>2. -00.0000000000</a></li>
-                                                </ul>
+
+                                            <div class="form-group">
+                                                <label for="phone">Teléfono</label>
+                                                <input type="text" name="phone" id="phone" class="form-control" placeholder="Teléfono" maxlength="10">
+                                                <div class="text-danger" id="div-phoneM"></div>
                                             </div>
-                                        </fieldset>
+                                            <div class="form-group">
+                                                <label for="images">Seleccione una imágen</label>
+                                                <input type="file" name="images" id="images" class="form-control"  maxlength="10">
+                                                <div class="text-danger" id="div-imageM"></div>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-md-6">
                                             
+                                            <fieldset>
+                                                <legend>Localización del Mecánico</legend>
+                                                <div class="form-group">
+                                                    <label for="lat">Latitud</label>
+                                                    <input type="text" name="lat" id="lat" class="form-control" placeholder="Latitud" maxlength="12">
+                                                    <div class="text-danger" id="div-latM"></div>
+                                                </div>
 
-                                        <input type="hidden" name="option" id="option" value="add">
-                                        <input type="hidden" name="update-id" id="update-id" value="">
-
-                                        <br><br>
-                                        
-                                    </div>
-                                    <div class="col-sm-6">
-                                        <div class="form-group">
-                                            <div id="servicesS">
+                                                <div class="form-group">
+                                                <label for="lon">Longitud</label>
+                                                    <input type="text" name="lon" id="lon" class="form-control" placeholder="Longitud" maxlength="12">
+                                                    <div class="text-danger" id="div-lonM"></div>
+                                                </div>
+                                                <div class="btn-group">
+                                                    <button type="button" class="btn-inf dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                        <span class="glyphicon glyphicon-info-sign"></span>
+                                                    </button>
+                                                    <ul class="dropdown-menu">
+                                                        <li><a href="#">El formato para la longitud y latitud debe ser como lo siguiente:<br>1. 00.00000000 (máximo 8 decimales)<br>2. -00.00000000</a></li>
+                                                    </ul>
+                                                </div>
+                                            </fieldset>
                                                 
-                                            </div>
-                                            <div class="text-danger" id="div-servM"></div>
-                                        </div>    
-                                    </div>
 
-                                    <div class="col-sm-6 hidden" id="checkinfo">
-                                        <div class="form-group">
-                                            <input type="checkbox" name="adicionar" id="adicionar"> Adicionar nuevos servicios seleccionados
+                                            <input type="hidden" name="option" id="option" value="add">
+                                            <input type="hidden" name="update-id" id="update-id" value="">
+
+                                            <br><br>
+                                            
                                         </div>
-                                        <div class="alert alert-info" role="alert">
-                                            <span class="glyphicon glyphicon-info-sign"></span> 
-                                            Si selecciona esta opción, además de los servicios con los que el mecánico ya cuenta, se agregarán los nuevos servicios elegidos.
-                                            En caso contrario, el mecánico solo tendrá los servicios que seleccione en este formulario.
+                                    </div>
+                                    
+                                    <div class="col-sm-12">
+                                        <div class="col-sm-6">
+                                            <div class="form-group">
+                                                <div id="servicesS">
+                                                    
+                                                </div>
+                                                <div class="text-danger" id="div-servM"></div>
+                                            </div>    
+                                        </div>
+
+                                        <div class="col-sm-6 hidden" id="checkinfo">
+                                            <div class="form-group">
+                                                <input type="checkbox" name="adicionar" id="adicionar"> Adicionar nuevos servicios seleccionados
+                                            </div>
+                                            <div class="alert alert-info" role="alert">
+                                                <span class="glyphicon glyphicon-info-sign"></span> 
+                                                Si selecciona esta opción, además de los servicios con los que el mecánico ya cuenta, se agregarán los nuevos servicios elegidos.
+                                                En caso contrario, el mecánico solo tendrá los servicios que seleccione en este formulario.
+                                            </div>
                                         </div>
                                     </div>
                                     <div class="col-sm-12 text-center">
@@ -145,17 +173,18 @@
 
                 </div>
             </div>
-        </div>                  
+        </div>
 
-    </article>
-</section>
+        
+        
+    </div>  <!--/.main-->
 
-<script src="js/jquery-3.1.1.min.js" type="text/javascript"></script>
-<script src="js/bootstrap.min.js" type="text/javascript"></script>
-<script src="js/bootbox.min.js" type="text/javascript"></script>
-<script src="js/mechanics.js" type="text/javascript"></script>
-<script src="js/logout.js" type="text/javascript"></script>
-<script src="js/destroysessiontimeout.js" type="text/javascript"></script>
-
+    <script src="js/jquery-1.11.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/bootbox.min.js" type="text/javascript"></script>
+    <script src="js/mechanics.js" type="text/javascript"></script>
+    <script src="js/logout.js" type="text/javascript"></script>
+    <script src="js/destroysessiontimeout.js" type="text/javascript"></script>
 </body>
+
 </html>
