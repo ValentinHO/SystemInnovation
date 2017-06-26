51c1196294faeb26cfdd45cb5ab415f147fc191a
ValentinHO <valentinhernandezortega@gmail.com>
2017-06-26 12:32:35 -0500

    Vista reportes 26-06-2017

--- reports.php
+++ reports.php
@@ -13,23 +13,35 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>System Innovation</title>
-    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
-    <link rel="icon" href="img/favicon.ico">
-    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1">
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-    <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" type="text/css" href="css/style.css">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>System Innovation</title>
+<link rel="icon" href="img/favicon.ico">
+
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link href="css/pagination.css" rel="stylesheet" type="text/css">
+<link href="css/styles.css" rel="stylesheet">
+
+<!--Icons-->
+<script src="js/lumino.glyphs.js"></script>
+
+<!--[if lt IE 9]>
+<script src="js/html5shiv.js"></script>
+<script src="js/respond.min.js"></script>
+<![endif]-->
+
 </head>
+
 <body id="reportes">
-
-<?php require('header.php');?>
-
-<section id="contenido">
-    <article>
-        <div class="title-modulo">
-            <p>Reporte de recomendación de Mecánicos</p>
-        </div>
+    <?php require('header.php');?>  
+    <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">           
+        <div class="row">
+            <ol class="breadcrumb">
+                <li><a href="main.php"><svg class="glyph stroked home"><use xlink:href="#stroked-home"></use></svg></a></li>
+                <li class="active">Reportes</li>
+            </ol>
+        </div><!--/.row-->
 
         <div class="col-lg-12">
             <div class="panel panel-primary">
@@ -46,15 +58,15 @@
             </div>
         </div>
         
-    </article>
-</section>
+        
+    </div>  <!--/.main-->
 
-<script src="js/jquery-3.1.1.min.js" type="text/javascript"></script>
-<script src="js/bootstrap.min.js" type="text/javascript"></script>
-<script src="js/bootbox.min.js" type="text/javascript"></script>
-<script src="js/reports.js" type="text/javascript"></script>
-<script src="js/logout.js" type="text/javascript"></script>
-<script src="js/destroysessiontimeout.js" type="text/javascript"></script>
-
+    <script src="js/jquery-1.11.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/bootbox.min.js" type="text/javascript"></script>
+    <script src="js/reports.js" type="text/javascript"></script>
+    <script src="js/logout.js" type="text/javascript"></script>
+    <script src="js/destroysessiontimeout.js" type="text/javascript"></script>
 </body>
+
 </html>
